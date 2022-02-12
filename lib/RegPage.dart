911f425b13f2e0d616594d011ad3120d571f1c18
kkkivannn <ikiselev288@gmail.com>
2022-02-12 23:28:13 +0300

    First commit

---
+++ lib/RegPage.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: file_names, camel_case_types, use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class regPage extends StatefulWidget {
+  @override
+  _regPageState createState() => _regPageState();
+}
+
+class _regPageState extends State<regPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xff6657A1),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'RegPage.dart';
+import 'Weather_JSON.dart';
+
+void main() {
+  runApp(regPage());
+}
