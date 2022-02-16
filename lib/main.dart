69e59c47301f5c57c1fee9482b4882e2da03c866
kkkivannn <ikiselev288@gmail.com>
2022-02-16 08:32:33 +0300

    Исправил одну маленькую ошибку, теперь все работает

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'Moscow.dart';
 import 'RegPage.dart';
-import 'WeatherEromTown.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: CountOfTownWeather(),
+      home: regPage(),
       debugShowCheckedModeBanner: false,
     ),
   );
